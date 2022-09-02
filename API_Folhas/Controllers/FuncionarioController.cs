bcff8e37b4dcc0e545370e93810326d85953d441
Aluno <Aluno>
2022-09-01 21:14:08 -0300

    API Folha - Alterar e Remover funcionário

--- API_Folhas/Controllers/FuncionarioController.cs
+++ API_Folhas/Controllers/FuncionarioController.cs
@@ -1,23 +1,22 @@
 using System.Collections.Generic;
+using System.Linq;
 using API_Folhas.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API_Folhas.Controllers
 {
-
     [ApiController]
     [Route("api/funcionario")]
     public class FuncionarioController : ControllerBase
     {
         private static List<Funcionario> funcionarios = new List<Funcionario>();
 
+        // GET: /api/funcionario/listar
         [Route("listar")]
         [HttpGet]
-        public IActionResult Listar()
-        {
-            return Ok(funcionarios);
-        }
+        public IActionResult Listar() => Ok(funcionarios);
 
+        // POST: /api/funcionario/cadastrar
         [Route("cadastrar")]
         [HttpPost]
         public IActionResult Cadastrar([FromBody] Funcionario funcionario)
@@ -26,18 +25,66 @@ namespace API_Folhas.Controllers
             return Created("", funcionario);
         }
 
+        // GET: /api/funcionario/buscar/123
         [Route("buscar/{cpf}")]
         [HttpGet]
         public IActionResult Buscar([FromRoute] string cpf)
         {
-            for (int i = 0; i < funcionarios.Count; i++)
+            //Expressão lambda
+            Funcionario funcionario = funcionarios.FirstOrDefault
+            (
+                f => f.Cpf.Equals(cpf)
+            );
+            //IF ternário
+            return funcionario != null ? Ok(funcionario) : NotFound();
+        }
+
+        // DELETE: /api/funcionario/deletar/123
+        [Route("deletar/{cpf}")]
+        [HttpDelete]
+        public IActionResult Deletar([FromRoute] string cpf)
+        {
+            Funcionario funcionario = funcionarios.FirstOrDefault
+            (
+                f => f.Cpf.Equals(cpf)
+            );
+            if (funcionario != null)
             {
-                if (funcionarios[i].Cpf.Equals(cpf))
-                {
-                    return Ok(funcionarios[i]);
-                }
+                funcionarios.Remove(funcionario);
+                return Ok(funcionario);
+            }
+            return NotFound();
+        }
+
+        // PATCH: /api/funcionario/alterar
+        [Route("alterar")]
+        [HttpPatch]
+        public IActionResult Alterar([FromBody] Funcionario funcionario)
+        {
+            Funcionario funcionarioBuscado = funcionarios.FirstOrDefault
+            (
+                f => f.Cpf.Equals(funcionario.Cpf)
+            );
+            if (funcionarioBuscado != null)
+            {
+                funcionarioBuscado.Nome = funcionario.Nome;
+                return Ok(funcionario);
             }
             return NotFound();
         }
     }
 }
+
+
+// if (funcionario != null)
+// {
+//     return Ok(funcionario);
+// }
+// return NotFound();
+// foreach (Funcionario f in funcionarios)
+// {
+//     if(f.Cpf.Equals(cpf))
+//     {
+//         return Ok(f);
+//     }
+// }
--- API_Folhas/Models/Funcionario.cs
+++ API_Folhas/Models/Funcionario.cs
@@ -4,6 +4,7 @@ namespace API_Folhas.Models
 {
     public class Funcionario
     {
+        public Funcionario() => CriadoEm = DateTime.Now;
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public DateTime Nascimento { get; set; }
